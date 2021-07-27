f97a7d5dff9af7ef7dfc9e3f5ad703fb8bd6e1fd
Nadine Coelho <nadinecoelho9@gmail.com>
2021-07-26 21:41:59 -0300

    update title of app

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>Nadine's Todo List</h1>
+        <h1>Todo List</h1>
       </header>
       <Form
         todos={todos}
